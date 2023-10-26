d9952549a10e1283b9d48219ec7eac216dad7289
Diego Santana <102614023+diegoednaldo@users.noreply.github.com>
2023-10-26 03:51:22 +0000

    commit do projeto

---
+++ firebaseatv/src/services/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcrYOnkHLvXfd8L1_vsInOUKqosZr6Taw",
+  authDomain: "fir-auth-c047f.firebaseapp.com",
+  projectId: "fir-auth-c047f",
+  storageBucket: "fir-auth-c047f.appspot.com",
+  messagingSenderId: "292973306906",
+  appId: "1:292973306906:web:232863b97008f054ab5da3",
+  measurementId: "G-SDBQ4TL73Z"
+};
+
+export const app = initializeApp(firebaseConfig);
